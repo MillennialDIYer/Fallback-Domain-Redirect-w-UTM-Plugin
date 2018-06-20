3f1e882ef6ce8a7f97662adb5b771d1420b329c7
Millennial DIYer <36612819+MillennialDIYer@users.noreply.github.com>
2018-06-19 21:39:07 -0400

    Minor description change.

--- Fallback-Domain-Redirect-w-UTM-Plugin/index.php
+++ Fallback-Domain-Redirect-w-UTM-Plugin/index.php
@@ -1,17 +1,15 @@
 <?php
-/**
- * index.php from Redirect Domain with UTM
- * Website here
+/* index.php from 'Fallback Domain Redirect with UTM' Plugin for YOURLS.
+ * https://github.com/MillennialDIYer/Fallback-Domain-Redirect-w-UTM-Plugin/
  *
- * For this page to function, the 'Fallback Domain Redirect with UTM' Plugin must be active.
- * 
  * Copy this into the base/root folder of your YOURLS install.
+ * For this page to function, the 'Fallback Domain Redirect with UTM' Plugin must be active, and a fallback domain saved in the settings.
 */
 
 // Start YOURLS engine
 require_once( dirname(__FILE__) . '/includes/load-yourls.php' );
 
-// Save the page URI to preserve any query string.
+// Save the incoming URL path to preserve any query string.
 fdr_save_uri() ;
 
 // Run fallback domain redirection function and redirect as appropriate.
